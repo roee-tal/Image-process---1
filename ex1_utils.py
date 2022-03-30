bb1aa48bdf3ce764012f5658efc28bc284e41c4b
roee-tal <roeetal979@gmail.com>
2022-03-30 20:49:46 +0300

    add comments

--- ex1_utils.py
+++ ex1_utils.py
@@ -46,16 +46,6 @@ def imReadAndConvert(filename: str, representation: int) -> np.ndarray:
     else:  # we should represent in RGB
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         return (img - img.min()) / (img.max() - img.min())
-    # image = cv2.imread(filename)
-    # if representation == 1:
-    #     im_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    #     # img_gray = NormalizeData(img_gray)
-    #     norm_image = cv2.normalize(im_gray, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-    #     return norm_image
-    # else:
-    #     im_RGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    #     norm_image = cv2.normalize(im_RGB, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-    #     return norm_image
 
 
 def imDisplay(filename: str, representation: int):
@@ -97,6 +87,11 @@ def transformYIQ2RGB(imgYIQ: np.ndarray) -> np.ndarray:
     return imgRGB
 
 def cumSum_calc(arr: np.array) -> np.ndarray:
+    """
+        help function to calculate the cumsum (want to try it by myself)
+        :param arr: Original Histogram
+        :return: the cumsum
+    """
     cum_sum = np.zeros_like(arr)
     cum_sum[0] = arr[0]
     arr_len = len(arr)
@@ -127,6 +122,11 @@ def hsitogramEqualize(imgOrig: np.ndarray) -> (np.ndarray, np.ndarray, np.ndarra
 
 
 def histEq2Shape(imgOrig):
+    """
+        help function to equalizes the histogram of an image
+        :param imgOrig: Original Histogram
+        :return: the new pic, the origin histogram, the new histogram
+    """
     # normalize from 0-1 to 0-255
     orig255 = cv2.normalize(imgOrig, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
     orig255 = orig255.astype(np.uint8)
@@ -151,10 +151,10 @@ def histEq2Shape(imgOrig):
 
 def find_q(z: np.array, image_hist: np.ndarray) -> np.ndarray:
     """
-        Calculate the new q using wighted average on the histogram
+        Help function to calculate the new q values for each border
         :param image_hist: the histogram of the original image
-        :param z: the new list of centers
-        :return: the new list of wighted average
+        :param z: border's list
+        :return: the new list of q
     """
     q = [np.average(np.arange(z[k], z[k + 1] + 1), weights=image_hist[z[k]: z[k + 1] + 1]) for k in range(len(z) - 1)]
     return np.round(q).astype(int)
@@ -162,14 +162,42 @@ def find_q(z: np.array, image_hist: np.ndarray) -> np.ndarray:
 
 def find_new_z(q: np.array) -> np.array:
     """
-        Calculate the new z using the formula from the lecture.
+        Help function to calculate the new borders using the formula from the lecture.
         :param q: the new list of q
-        :param z: the old z
-        :return: the new z
+        :return: the new borders
     """
     z_new = np.array([round((q[i - 1] + q[i]) / 2) for i in range(1, len(q))]).astype(int)
     z_new = np.concatenate(([0], z_new, [255]))
     return z_new
+
+def find_first_z(pixel_num, nQuant, histOrig):
+    """
+         Help function to find the first borders - according to the pixel's amount - same amount in each part
+        :param pixel_num: pixel's amount
+        :param nQuant: Number of colors to quantize the image to
+        :param histOrig: The original histogram
+        :return: (List[qImage_i],List[error_i])
+    """
+    # In order to put same amount in each part - i used the cumsum
+    cumsum = np.cumsum(histOrig)
+    new_z =np.zeros(nQuant + 1, dtype=int)
+    bound1 = pixel_num / nQuant
+    bound = bound1
+    i = 1
+    for x in range(255):
+        # check which gray rank is crossing the bound, for each bound
+        if (cumsum[x] >= bound):
+            new_z[i] = x
+            i = i + 1
+            bound = bound + bound1
+    while (i < len(new_z) - 1):
+        # add the first and last borders
+        new_z[i] = 255
+        i = i + 1
+    new_z[0] = 0
+    new_z[len(new_z) - 1] = 255
+    new_z = new_z.astype(int)
+    return new_z
 
 def quantizeImage(imOrig: np.ndarray, nQuant: int, nIter: int) -> (List[np.ndarray], List[float]):
     """
@@ -179,34 +207,50 @@ def quantizeImage(imOrig: np.ndarray, nQuant: int, nIter: int) -> (List[np.ndarr
         :param nIter: Number of optimization loops
         :return: (List[qImage_i],List[error_i])
     """
-    if len(imOrig.shape) == 2:
+    if len(imOrig.shape) == 2: # grayscale
         flag = 2
         return Quant2Shape(imOrig.copy(), nQuant, nIter, flag, 0)
 
-    flag = 3
+    flag = 3 # rgb - operate on the Y channel of YIQ
     yiqImg = transformRGB2YIQ(imOrig)
     return Quant2Shape(yiqImg[:, :, 0].copy(), nQuant, nIter, flag, yiqImg)  # y channel = yiqImg[:, :, 0].copy()
 
 
 def Quant2Shape(imOrig, nQuant, nIter, flag, img):
+    """
+        Help function to quantize an image with shape 2 in to **nQuant** colors
+        :param imOrig: The original image (RGB or Gray scale)
+        :param nQuant: Number of colors to quantize the image to
+        :param nIter: Number of optimization loops
+        :param flag: the shape of the image
+        :param img: The whole img(relevant for rgb case)
+        :return: (List[qImage_i],List[error_i])
+    """
     Q_IM = []
     MSE = []
+    # normalize from 0-1 to 0-255
     orig255 = cv2.normalize(imOrig, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
     orig255 = orig255.astype(np.uint8)
+    # calculate the histogram
     histOrig255 = np.zeros(256)
     for pix in range(256):
         histOrig255[pix] = np.count_nonzero(orig255 == pix)
     pixel_num = (float)(imOrig.shape[0] * imOrig.shape[1])
+    # find the first z values
     z_border = find_first_z(pixel_num,nQuant,histOrig255)
     for i in range(nIter):
+        # find the new q values
         q = find_q(z_border, histOrig255)
         qImage_i = np.zeros_like(imOrig)
+        # update the color to be the mean color for each part
         for k in range(len(q)):
             qImage_i[orig255 > z_border[k]] = q[k]
-
         z_border = find_new_z(q)
+        # calculate mse
         MSE.append(np.sqrt((orig255 - qImage_i) ** 2).mean())
+        # add the normalized [0,1] pic to the list
         Q_IM.append(qImage_i / 255.0)
+    # In case of rgb - transform back to rgb from yiq
     if flag == 3:
         for i in range(len(Q_IM)):
             img[:, :, 0] = Q_IM[i]
@@ -216,21 +260,3 @@ def Quant2Shape(imOrig, nQuant, nIter, flag, img):
     return Q_IM, MSE
 
 
-def find_first_z(pixel_num, nQuant, histOrig):
-    cumsum = np.cumsum(histOrig)
-    new_z =np.zeros(nQuant + 1, dtype=int)
-    bound1 = pixel_num / nQuant
-    bound = bound1
-    i = 1
-    for x in range(255):
-        if (cumsum[x] >= bound):
-            new_z[i] = x
-            i = i + 1
-            bound = bound + bound1
-    while (i < len(new_z) - 1):
-        new_z[i] = 255
-        i = i + 1
-    new_z[0] = 0
-    new_z[len(new_z) - 1] = 255
-    new_z = new_z.astype(int)
-    return new_z
